f4bb2efde866999909e4bd01513dcde193231874
irb <yadagiritandu@gmail.com>
2015-07-07 00:24:25 -0700

    adds basic directives

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,20 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
     {'name': 'MOTOROLA XOOM™','snippet': 'The Next, Next Generation tablet.'}
     ];
     $scope.val = 1
+    $scope.even = false
+    $scope.myarr = [1,2,3,4,5,5]
+    $scope.users = [{'name':'user1'},{'name':'user2'},{'name':'user3'}]
+
     $scope.inc = function(){
         $scope.val +=1
+        $scope.even = $scope.val%2==0
     }
+
+    $scope.isEven = function(){
+       return $scope.val%2 == 0
+    }
+
+    $scope.obj = {name:'name1',age:'age1',title:'Mr.'}
+
+    $scope.classVar = 'orange'
 });
